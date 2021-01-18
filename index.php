17238db41d7b6509f2c4dfa9a96ac56687e0aa5a
dante <dantinr@gmail.com>
2021-01-18 11:04:47 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+    include "pdo.php";
+
+    $pdo = getPdo();
+    //获取房间列表
+    $sql = "select * from p_rooms";
+    $res = $pdo->query($sql);
+    $data = $res->fetchAll(PDO::FETCH_ASSOC);
+    //echo '<pre>';print_r($data);echo '</pre>';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>老王会所</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+<hr>
+<h1>房间预订</h1>  <span id="time"></span>
+<div id="app">
+    
+    <?php
+        foreach ($data as $k=>$v){
+            $price = $v['price'] / 100 . '.00';
+            $id = $v['id'];
+            echo "<div class='seat  green ' data-id='{$id}' data-status='0'>";
+            echo "<button class='btn' >可预订 ¥{$price}</button>";
+            echo "<h1 class='nickname'>{$v['name']}</h1>";
+            echo "</div>";
+        }
+    ?>
+
+</div>
+
+
+<script src="/js/jquery-3.5.1.min.js"></script>
+<script src="/js/time.js"></script>
+<script src="/js/seat.js"></script>
+
+</body>
+</html>
